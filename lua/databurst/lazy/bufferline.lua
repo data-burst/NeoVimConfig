c198976d2694f09b53d7434e430cc4a5bb3eaed5
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-16 11:22:02 -0500

    :wrench: chore: add command for closing current buffer

--- lua/databurst/lazy/bufferline.lua
+++ lua/databurst/lazy/bufferline.lua
@@ -4,21 +4,26 @@ return {
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require("bufferline").setup({
-        options = {
-          diagnostics = "nvim_lsp",
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              text_align = "center",
-              highlight = "Directory",
-            }
-          },
+      options = {
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center",
+            highlight = "Directory",
+          }
+        },
       }
     })
 
-    vim.keymap.set("n", "<A-,>", ":BufferLineCyclePrev<CR>", { silent = true })
-    vim.keymap.set("n", "<A-.>", ":BufferLineCycleNext<CR>", { silent = true })
+    vim.keymap.set("n", "<A-,>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<A-.>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<A-c>", function()
+      local current_buf = vim.api.nvim_get_current_buf()
+      vim.cmd("bnext") -- Move to next buffer
+      vim.api.nvim_buf_delete(current_buf, { force = false })
+    end
+    , { noremap = true, silent = true })
   end
 }
-
