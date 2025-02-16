55f3303f1c336cbc81b5e506fb97e7def3eeab3e
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-15 22:25:28 -0500

    :hammer: chore: add default global settings

---
+++ lua/databurst/set.lua
@@ -0,0 +1,11 @@
+vim.cmd.colorscheme("catppuccin") -- set colorscheme to catppuccin
+
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+
+vim.opt.relativenumber = true -- use relative line numbers
+
+vim.opt.tabstop = 2 -- set tab size to 2 spaces
+vim.opt.softtabstop = 2 -- set tab size to 2 spaces
+vim.opt.shiftwidth = 2 -- set indent size to 2 spaces
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.smartindent = true -- autoindent based on previous line
