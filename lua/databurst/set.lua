3491894bb36eb59a54029c1d9758bc3998459d60
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-17 12:47:59 -0500

    :wrench: chore: set line number option

--- lua/databurst/set.lua
+++ lua/databurst/set.lua
@@ -9,3 +9,4 @@ vim.opt.softtabstop = 2 -- set tab size to 2 spaces
 vim.opt.shiftwidth = 2 -- set indent size to 2 spaces
 vim.opt.expandtab = true -- use spaces instead of tabs
 vim.opt.smartindent = true -- autoindent based on previous line
+vim.opt.number = true -- show line numbers
