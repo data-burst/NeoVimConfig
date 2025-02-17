3a8dd2e5119fc3eb6d539ff477772fa597f4a1f3
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-17 12:37:24 -0500

    :wrench: chore: make line numbers absolute

--- lua/databurst/set.lua
+++ lua/databurst/set.lua
@@ -2,7 +2,7 @@ vim.cmd.colorscheme("catppuccin") -- set colorscheme to catppuccin
 
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
 
-vim.opt.relativenumber = true -- use relative line numbers
+vim.opt.relativenumber = false -- use relative line numbers
 
 vim.opt.tabstop = 2 -- set tab size to 2 spaces
 vim.opt.softtabstop = 2 -- set tab size to 2 spaces
